d573499a61d05be8bbe6ea71d448a3944c0a6506
User <80336639062@mail.ru>
2023-02-18 15:18:04 +0300

    dz_djando_1

--- Alex_2023/project/urls.py
+++ Alex_2023/project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from. import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.project)
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
