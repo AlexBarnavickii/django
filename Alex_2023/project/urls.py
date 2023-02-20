6f25ef639905044bee0ac54bfce5ee6766dd093c
User <80336639062@mail.ru>
2023-02-20 23:40:51 +0300

    dz_djando_1

--- Alex_2023/project/urls.py
+++ Alex_2023/project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+app_name = 'projects'
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
     path("blog", views.blog_index, name="blog_index"),
-    path("<int:pk>/", views.blog_detail, name="blog_detail")
+    path("<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("<int:pk>/comments/", views.comments, name="comments")
 ]
--- Alex_2023/project/views.py
+++ Alex_2023/project/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
 from .models import Project
 from .models import Comment
-
-
+from django.http import HttpResponse
+from django.urls import reverse
 def project_index(request):
     projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
+    context = {'projects': projects}
+
     return render(request, 'project_index.html', context)
 
 
 def project_detail(request, pk):
+    a = Project.objects.get(id=pk)
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
+    context = {'project': project}
+    comments_list = a.comment_set.order_by('-id')
+    return render(request, 'project_detail.html',  {'comments_list' : comments_list})
 
 
 #####################################################
 def blog_index(request):
-    projects = Comment.objects.all()
+    projects = Comment.objects.order_by('-cteated_on')
     context = {
         'projects': projects
     }
-    return render(request, 'blog_index.html', context)
+    return render(request, 'blog_index.html', {'projects': projects})
 
 def blog_detail(request, pk):
     project = Comment.objects.get(pk=pk)
@@ -34,4 +33,7 @@ def blog_detail(request, pk):
     }
     return render(request, 'blog_detail.html', context)
 
-
+def comments(request, project_id):
+    a = Project.objects.get(id = project_id)
+    a.comments_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+    return HttpResponse('project:project_detail', args = (a.id,))
