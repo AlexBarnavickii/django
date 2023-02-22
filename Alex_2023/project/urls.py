daf15d670ccf7420f31e404593225d1a214d1b60
User <80336639062@mail.ru>
2023-02-22 22:46:30 +0300

    dz_djando_1

--- Alex_2023/project/urls.py
+++ Alex_2023/project/urls.py
@@ -5,7 +5,8 @@ app_name = 'projects'
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
-    path("blog", views.blog_index, name="blog_index"),
-    path("<int:pk>/", views.blog_detail, name="blog_detail"),
-    path("<int:pk>/comments/", views.comments, name="comments")
+    path("blog/", views.blog_index, name="blog_index"),
+    path("<int:id>/", views.blog_detail, name="blog_detail_1"),
+    path("azs/", views.blog_detail, name="azs")
+    # path("<int:pk>/comments/", views.comments, name="comments")
 ]
--- Alex_2023/project/views.py
+++ Alex_2023/project/views.py
@@ -11,11 +11,11 @@ def project_index(request):
 
 
 def project_detail(request, pk):
-    a = Project.objects.get(id=pk)
     project = Project.objects.get(pk=pk)
-    context = {'project': project}
-    comments_list = a.comment_set.order_by('-id')
-    return render(request, 'project_detail.html',  {'comments_list' : comments_list})
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
 
 
 #####################################################
@@ -26,14 +26,17 @@ def blog_index(request):
     }
     return render(request, 'blog_index.html', {'projects': projects})
 
-def blog_detail(request, pk):
-    project = Comment.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
+def blog_detail(request):
+    project = Comment.objects.all()
+    context = {'project': project}
     return render(request, 'blog_detail.html', context)
 
 def comments(request, project_id):
     a = Project.objects.get(id = project_id)
     a.comments_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
     return HttpResponse('project:project_detail', args = (a.id,))
+
+def azs(request):
+    project = Comment.objects.all()
+    context = {'project': project}
+    return render(request, 'blog_detail.html', context)
